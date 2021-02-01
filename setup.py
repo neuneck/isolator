d3e23a063ffa112d551e23f96b836700621aa835
Julian Schweizer <js@nect.com>
2021-02-01 13:50:12 +0100

    Improved setup.py arguments

--- setup.py
+++ setup.py
@@ -4,11 +4,21 @@ from setuptools import setup
 
 version = "0.1.0"
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="isolator",
     version=version,
+    license="mit",
     description="Isolate a python class into a separate process",
+    long_description=long_description,
     packages=["isolator"],
-    python_requires=">=3.6",
-    install_requires=[]
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+    python_requires='>=3.6',
 )
